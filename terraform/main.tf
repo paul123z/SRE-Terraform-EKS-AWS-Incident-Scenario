a9633da67fb7c0205cee34d552263ec208c59884
paul123z <palolantaj@gmail.com>
2025-08-05 20:42:22 +0200

    Fix duplicate output definitions in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,28 +83,4 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
   role       = module.eks.eks_managed_node_groups["main"].iam_role_name
 }
 
-# Outputs
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = module.eks.cluster_security_group_id
-}
-
-output "cluster_iam_role_name" {
-  description = "IAM role name associated with EKS cluster"
-  value       = module.eks.cluster_iam_role_name
-}
-
-output "cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.eks.cluster_certificate_authority_data
-}
-
-output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.cluster_name
-} 
+ 
